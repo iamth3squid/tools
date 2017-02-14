0406b8e6a2434f191689f99c51f99e90af982cbb
iamth3squid <iamth3squid@gmail.com>
2017-02-13 22:49:08 -0600

    Create updateupgrade.sh

---
+++ updateupgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#updateupgrade.sh
+#2/13/2017
+#created by Iamth3squid
+
+#updates and upgrades so you dont have to keep using the commands for ubuntu 16.04
+
+
+#update
+sudo apt-get upgrade -y && sudo apt-get update -y
+
+#Check again
+sudo apt-get upgrade -y
+sudo apt-get update -y
+
