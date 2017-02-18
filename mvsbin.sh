695b754b4fbc2d040da021705f23454c7c4d74f1
iamth3squid <iamth3squid@gmail.com>
2017-02-17 20:40:34 -0600

    Update mvsbin.sh

--- mvsbin.sh
+++ mvsbin.sh
@@ -8,3 +8,27 @@
 # EX: mvsbin.sh MYSCRIPT
 
 
+if [ -z "$1" ]; then
+
+
+        echo "You must name the file in the first option"
+       
+else
+
+        SCRIPTTOMOVE=$1
+        
+
+        # add script stuff
+        sudo mv $1 /usr/local/sbin
+        echo "File moved\n"
+   
+        
+        # Add Execute permissions
+        chmod +x $1
+
+        # Finish
+        echo "File moved and added The excute permission"
+
+
+fi
+
