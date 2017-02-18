a22c8eb6fea049399bdc453b2353ba73db123b11
iamth3squid <iamth3squid@gmail.com>
2017-02-17 20:20:07 -0600

    Create mvsbin.sh

---
+++ mvsbin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#mvsbin.sh
+#2/17/2017
+#moves script to sbin and makes it excutable 
+
+
+#Need to give the name of the script you want to move as the first option
+# EX: mvsbin.sh MYSCRIPT
+
+
