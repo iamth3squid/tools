86a4bb61208c32133d15c0168f05d68daefd19cf
iamth3squid <iamth3squid@gmail.com>
2018-04-08 12:52:17 -0500

    Update cpsbin.sh
    edited comments to reflect changes in script

--- cpsbin.sh
+++ cpsbin.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#mvsbin.sh
+#cpsbin.sh
 #2/17/2017
 #moves script to sbin and makes it excutable 
 
 
 #Need to give the name of the script you want to move as the first option
-#EX: mvsbin.sh MYSCRIPT
+#EX: cpsbin.sh MYSCRIPT
 
 
 if [ -z "$1" ]; then
